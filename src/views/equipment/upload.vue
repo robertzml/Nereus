0a1107454def21c208000058007beff95304307f
robertzml <robertzml@126.com>
2018-03-17 14:42:37 +0800

    add upload equipment to store.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ src/views/equipment/upload.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <Row>
+            <Col span="24">
+                <Card>
+                     <p slot="title">
+                        <Icon type="grid"></Icon>
+                        上传设备数据
+                    </p>
+
+                    <Upload name="file" 
+                        :action="uploadUrl" 
+                        :headers="header" 
+                        accept=".txt" 
+                        :format="['txt']" 
+                        :show-upload-list="false" 
+                        :withCredentials="true"
+                        :on-success="handleSuccess"
+                        :on-error="handleError">
+                        <Button type="ghost" icon="ios-cloud-upload-outline">Upload files</Button>
+                    </Upload>
+                </Card>
+            </Col>
+        </Row>
+    </div>
+</template>
+
+<script>
+import config from '../../utility/config.js'
+
+export default {
+    name: 'equipment-upload',
+    data () {
+        return {
+            header: {
+                
+            },
+            uploadUrl: config.apihost + 't_store/addStoreByCompanyTxt/uploadTxt'
+        }
+    },
+    methods: {
+        handleSuccess (res, file) {
+            this.$Message.info(res.message)
+        },
+
+        handleError (err, file) {
+            console.log(err)
+            this.$Message.error('上传文件失败')
+        }
+    }
+}
+</script>
+
