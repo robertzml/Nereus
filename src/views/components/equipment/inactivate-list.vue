02b0c2b19742c3418c85b8b8f1832833c2921e8e
robertzml <robertzml@126.com>
2018-06-12 20:08:24 +0800

    add inactivate list.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ src/views/components/equipment/inactivate-list.vue
@@ -0,0 +1,153 @@
+<template>
+    <div class="equipment-list">
+        <Table :data="tableData" :columns="columns" border stripe></Table>
+        <div style="margin: 10px;overflow-x: hidden" v-if="showPager">
+            <div style="float: right;">
+                <Page :total="itemsCount" :current.sync="currentPage" :page-size="pageSize" :page-size-opts="pageSizeOpt" show-sizer placement="top" 
+                    @on-page-size-change="changePageSize"></Page>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import * as nereus from '@/utility/nereus.js'
+import equipment from '@/controllers/equipment.js'
+
+export default {
+    name: 'inactivate-list',
+    props: {
+        itemList: { type: Array, required: true },
+        showPager: { type: Boolean, default: true }
+    },
+    data () {
+        return {
+            columns: [
+                {
+                    type: 'index',
+                    width: 60,
+                    align: 'center'
+                },
+                {
+                    title: '序列号',
+                    key: 'serial_number'
+                },
+                {
+                    title: '申请人',
+                    key: 'apply_account_name'
+                },
+                {
+                    title: '代理商',
+                    key: 'agent_company_name'
+                },
+                {
+                    title: '用户姓名',
+                    key: 'owner_name'
+                },
+                {
+                    title: '用户电话',
+                    key: 'owner_phone'
+                },
+                {
+                    title: '激活状态',
+                    key: 'auditing_state'
+                },
+                {
+                    title: '申请时间',
+                    key: 'create_date',
+                    render: (h, params) => {
+                        return (
+                            <span>{ nereus.displayDateTime(params.row.create_date) }</span>
+                        )
+                    }
+                },
+                {
+                    title: '操作',
+                    key: 'action',
+                    width: 150,
+                    align: 'center',
+                    render: (h, params) => {
+                        return (
+                            <div>
+                                <i-button type="primary" size="small" style="marginRight: 5px" onClick={ () => { this.activate(params.row) } }>激活</i-button>
+                                <i-button type="warning" size="small" style="marginRight: 5px" onClick={ () => { this.reject(params.row) } }>驳回</i-button>
+                            </div>
+                        )
+                    }
+                }
+            ],
+            pageSize: 10,
+            currentPage: 1,
+            pageSizeOpt: [5, 10, 20, 30]
+        }
+    },
+    computed: {
+        itemsCount () {
+            return this.itemList.length
+        },
+        tableData () {
+            let temp = this.itemList
+            
+            let pageCount = Math.ceil(temp.length / this.pageSize)
+
+            if (this.currentPage > pageCount) {
+                this.changePage(1)
+            }
+            return temp.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize)
+        }
+    },
+    methods: {
+        changePage (page) {
+            this.currentPage = page
+        },
+        changePageSize (pageSize) {
+            this.pageSize = pageSize
+        },
+        activate (item) {
+            let act = [{
+                serial_number: item.serial_number,
+                is_activate: 1
+            }]
+       
+            equipment.activation(act).then(res => {
+                if (res.status === 0) {
+                    this.$Notice.success({
+                        title: '激活成功',
+                        desc: res.message
+                    })
+
+                    this.getData()
+                } else {
+                    this.$Notice.error({
+                        title: '激活失败',
+                        desc: res.message
+                    })
+                }
+            })
+        },
+
+        reject (item) {
+            let act = [{
+                serial_number: item.serial_number,
+                is_activate: 0
+            }]
+       
+            equipment.activation(act).then(res => {
+                if (res.status === 0) {
+                    this.$Notice.success({
+                        title: '激活已驳回',
+                        desc: res.message
+                    })
+
+                    this.getData()
+                } else {
+                    this.$Notice.error({
+                        title: '驳回失败',
+                        desc: res.message
+                    })
+                }
+            })
+        }
+    }
+}
+</script>
--- src/views/equipment/inactivate.vue
+++ src/views/equipment/inactivate.vue
@@ -10,12 +10,8 @@
                     <Icon type="ios-loop-strong"></Icon>
                     刷新
                 </a>
-                <Table :data="tableData" :columns="columns" border stripe></Table>
-                <div style="margin: 10px;overflow-x: hidden">
-                    <div style="float: right;">
-                        <Page :total="itemsCount" :current.sync="currentPage" :page-size="pageSize" show-sizer @on-change="changePage"></Page>
-                    </div>
-                </div>
+
+                <inactivate-list :item-list="inactivateData"></inactivate-list>
             </Card>
         </Col>
     </Row>
@@ -24,72 +20,17 @@
 <script>
 import equipment from '@/controllers/equipment.js'
 import * as nereus from '@/utility/nereus.js'
+import inactivateList from '../components/equipment/inactivate-list.vue'
 import _ from 'lodash'
 
 export default {
     name: 'equipment-inactivate',
+    components: {
+        inactivateList
+    },
     data () {
-        return {
-            columns: [
-                {
-                    type: 'index',
-                    width: 60,
-                    align: 'center'
-                },
-                {
-                    title: '序列号',
-                    key: 'serial_number'
-                },
-                {
-                    title: '申请人',
-                    key: 'apply_account_name'
-                },
-                {
-                    title: '代理商',
-                    key: 'agent_company_name'
-                },
-                {
-                    title: '用户姓名',
-                    key: 'owner_name'
-                },
-                {
-                    title: '用户电话',
-                    key: 'owner_phone'
-                },
-                {
-                    title: '激活状态',
-                    key: 'auditing_state'
-                },
-                {
-                    title: '申请时间',
-                    key: 'create_date',
-                    render: (h, params) => {
-                        return (
-                            <span>{ nereus.displayDateTime(params.row.create_date) }</span>
-                        )
-                    }
-                },
-                {
-                    title: '操作',
-                    key: 'action',
-                    width: 150,
-                    align: 'center',
-                    render: (h, params) => {
-                        return (
-                            <div>
-                                <i-button type="primary" size="small" style="marginRight: 5px" onClick={ () => { this.activate(params.row) } }>激活</i-button>
-                                <i-button type="warning" size="small" style="marginRight: 5px" onClick={ () => { this.reject(params.row) } }>驳回</i-button>
-                            </div>
-                        )
-                    }
-                }
-            ],
-            items: [],
-            itemsCount: 0,
-            tableData: [],
-            currentPage: 1,
-            pageSize: 10,
-            pageSizeOpt: [5, 10, 20, 30]
+        return {            
+            inactivateData: []
         }
     },
     methods: {
@@ -104,10 +45,7 @@ export default {
             if (roleType === 0 || roleType === 1) {
                 equipment.getInactivate().then(res => {
                     if (res.status === 0) {
-                        vm.items = res.entities
-                        vm.itemsCount = res.entities.length
-                        vm.currentPage = 1
-                        vm.tableData = _.slice(vm.items, 0, vm.pageSize)
+                        vm.inactivateData = res.entities
                     } else {
                         this.$Notice.error({
                             title: '获取设备信息失败',
@@ -120,10 +58,7 @@ export default {
 
                 equipment.getInactivate(companyId).then(res => {
                     if (res.status === 0) {
-                        vm.items = res.entities
-                        vm.itemsCount = res.entities.length
-                        vm.currentPage = 1
-                        vm.tableData = _.slice(vm.items, 0, vm.pageSize)
+                        vm.inactivateData = res.entities
                     } else {
                         this.$Notice.error({
                             title: '获取设备信息失败',
@@ -132,10 +67,6 @@ export default {
                     }
                 })
             }
-        },
-
-        changePage (page) {
-            this.tableData = _.slice(this.items, (page - 1) * this.pageSize, page * this.pageSize)
         },
 
         activate (item) {
